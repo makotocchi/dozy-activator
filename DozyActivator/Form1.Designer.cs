3f6860f4bb1445a61343c8241f3cdf7943e3e56f
Renam Aono <Renam Aono@LAPTOP-RENAM>
2016-08-31 23:16:16 -0300

    Add project files.

---
+++ DozyActivator/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DozyActivator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.buttonToggle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 58);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Choose file";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(12, 23);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.ReadOnly = true;
+            this.textBoxFile.Size = new System.Drawing.Size(331, 20);
+            this.textBoxFile.TabIndex = 1;
+            // 
+            // buttonToggle
+            // 
+            this.buttonToggle.Location = new System.Drawing.Point(268, 58);
+            this.buttonToggle.Name = "buttonToggle";
+            this.buttonToggle.Size = new System.Drawing.Size(75, 23);
+            this.buttonToggle.TabIndex = 2;
+            this.buttonToggle.Text = "Toggle";
+            this.buttonToggle.UseVisualStyleBackColor = true;
+            this.buttonToggle.Click += new System.EventHandler(this.buttonToggle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 94);
+            this.Controls.Add(this.buttonToggle);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.buttonOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dozy Activator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Button buttonToggle;
+    }
+}
+
---
+++ DozyActivator/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DozyActivator
+{
+    public partial class Form1 : Form
+    {
+        enum DozyState : byte { Off = 6, On };
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "TR4 Script Files (*.DAT)|SCRIPT.DAT";
+            buttonToggle.Enabled = false;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFile.Text = openFileDialog1.FileName;
+                if (textBoxFile.Text.ToLower().EndsWith("script.dat"))
+                {
+                    buttonToggle.Enabled = true;
+                }
+            }  
+        }
+
+        private void buttonToggle_Click(object sender, EventArgs e)
+        {
+            string fileName = openFileDialog1.FileName;
+            DozyState state = toggleDozy(fileName);
+            if(state == DozyState.On)
+            {
+                MessageBox.Show("Dozy on!");
+            }
+            else
+            {
+                MessageBox.Show("Dozy off.");
+            }
+        }
+
+        private static DozyState toggleDozy(string fileName)
+        {
+            byte[] data = new byte[1];
+            using (Stream stream = File.Open(fileName, FileMode.Open))
+            {
+                stream.Read(data, 0, data.Length);
+                if((DozyState) data[0] == DozyState.Off)
+                {
+                    data[0] = (byte) DozyState.On;
+                }
+                else
+                {
+                    data[0] = (byte) DozyState.Off;
+                }
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.Write(data, 0, data.Length);
+            }
+            return (DozyState) data[0];
+        } 
+    }
+}
